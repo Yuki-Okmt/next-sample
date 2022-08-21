0006e8be16fdc9baa902ca2f63a98f7966331699
Yuki-Okmt <y.okamotty+github@gmail.com>
2022-08-21 18:53:34 +0900

    apiコールしてみた

---
+++ pages/sayhello.tsx
@@ -0,0 +1,22 @@
+import { useState, useEffect } from "react";
+
+function Sayhello() {
+    // 内部で状態を保つためuseStateを利用
+    const [data, setData] = useState({ name: '' });
+    // 外部のAPIにリクエストするのは副作用なのでuseEffect内で処理　
+
+    useEffect(() => {
+        // pages/api/hello.tsの内容にリクエスト
+        fetch('api/hello')
+            .then((res) => {
+                console.log(res);
+                return res.json();
+            })
+            .then((profile) => {
+                setData(profile)
+            })
+    }, [])
+    return <div>hello {data.name}</div>
+}
+
+export default Sayhello;
